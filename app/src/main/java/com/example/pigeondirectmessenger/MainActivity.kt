8dccc93ba80a3e1a852f413ff7f9128cd8e15936
naah16 <naaraholiveira@gmail.com>
2022-12-12 13:05:20 -0300

    adicionando mensagens recebidas e enviadas

--- app/src/main/java/com/example/pigeondirectmessenger/MainActivity.kt
+++ app/src/main/java/com/example/pigeondirectmessenger/MainActivity.kt
@@ -5,20 +5,38 @@ import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
+const val USER_ID = 0
+const val OTHER_ID = 1
+
 class MainActivity : AppCompatActivity() {
+    private var fromUser = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        send_button.setOnClickListener{
-            val message = message_editText.text.toString()
-            message_editText.setText("")
-            //message_textView.text = message
-        }
+        setUpListeners()
+        setUpRecyclerView()
 
+    }
+
+    private fun setUpListeners() {
+        send_button.setOnClickListener{
+            val messageText = message_editText.text.toString()
+            message_editText.setText("")
+
+            val adapter = message_list.adapter
+            if(adapter is MessageAdapter) {
+                val message = ChatMessage(messageText, if(fromUser) USER_ID else OTHER_ID)
+                adapter.addItem(message)
+                message_list.scrollToPosition(adapter.itemCount-1)
+                fromUser = !fromUser
+            }
+        }
+    }
+
+    private fun setUpRecyclerView() {
         //configura o RecyclerView
         message_list.layoutManager = LinearLayoutManager(this)
         message_list.adapter = MessageAdapter()
-
     }
 }
--- app/src/main/java/com/example/pigeondirectmessenger/MessageAdapter.kt
+++ app/src/main/java/com/example/pigeondirectmessenger/MessageAdapter.kt
@@ -4,31 +4,50 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.appcompat.view.menu.MenuView
 import androidx.recyclerview.widget.RecyclerView
+
+const val SENT_MESSAGE = 0
+const val RECEIVED_MESSAGE = 1
 
 class MessageAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     //lista de mensagens
-    val items: List<String> = listOf()
+    private val items: MutableList<ChatMessage> = mutableListOf()
+
+    fun addItem(message: ChatMessage) {
+        items.add(message)
+        notifyDataSetChanged()
+    }
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
-        LayoutInflater.from(parent.context).inflate(R.layout.message_card, parent, false)
+
+        val card = LayoutInflater
+            .from(parent.context)
+            .inflate(
+                if(viewType == SENT_MESSAGE)
+                    R.layout.send_card
+                else R.layout.received_card, parent, false)
+        return MessageViewHolder(card)
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+
         val currentItem = items[position]
         if(holder is MessageViewHolder) {
-            holder.messageTextView.text = currentItem
+            holder.messageTextView.text = currentItem.text
         }
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-        return items.size
-    }
+    override fun getItemCount() = items.size
+
+
+    override fun getItemViewType(position: Int) =
+        if(items[position].senderId == USER_ID) {
+            SENT_MESSAGE
+        } else {
+            RECEIVED_MESSAGE
+        }
+
 
     class MessageViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         val messageTextView: TextView = itemView.findViewById(R.id.message_textView)
